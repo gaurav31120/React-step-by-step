16b3493ec3aa08013805217adcf960c3d7891b71
Gaurav Kumar <gaurav31120@gmail.com>
2025-07-09 02:50:57 +0530

    2 Creating a component in app.jsx

--- react-step-by-step/src/App.jsx
+++ react-step-by-step/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import Header from "./Header";
 
 function App() {
-
   return (
     <>
-    <h1>React step by step</h1>
-    <h1>Hello React 19</h1>
+      <Header />
+      <h1>Hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+//     <h1>React step by step</h1>
+//     <h1>Hello React 19</h1>
+//     </>
+//   )
+// }
+
+// export default App
